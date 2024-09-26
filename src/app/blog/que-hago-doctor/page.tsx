a65d03f43e59820eb2cc55614d7fb722ee672e70
julian <jdvs_g12@hotmail.com>
2024-09-26 18:08:10 -0500

    fix

--- src/app/blog/que-hago-doctor/page.tsx
+++ src/app/blog/que-hago-doctor/page.tsx
@@ -7,13 +7,11 @@ import type { Metadata } from "next";
 
 
 export const metadata: Metadata = {
-    title: "Psicología y Cultura | ¿Qué hago doctor?",
-    description: "Cualquier camino es imposible cuando se espera que sea otro el que camine. Esta es una confesión, la hago al inicio de este blog ahora más estructurado que hace nueve años. Era 2015 cuando decidí iniciar alguna formación en Psicoanálisis en Buenos Aires (Argentina), para ese entonces ya era profesional en Psicología y tenía 23 años.",
-    keywords: 'Psicoanálisis, Psicoterapia, Ser psicoanalista, Freud y Lacan, Salud mental, Autoconocimiento, Sufrimiento emocional, Análisis personal, Supervisión clínica ,Transformación personal',
-
+    openGraph: {
+        title: "Psicología y Cultura | ¿Qué hago doctor?",
+        description: "Cualquier camino es imposible cuando se espera que sea otro el que camine. Esta es una confesión, la hago al inicio de este blog ahora más estructurado que hace nueve años. Era 2015 cuando decidí iniciar alguna formación en Psicoanálisis en Buenos Aires (Argentina), para ese entonces ya era profesional en Psicología y tenía 23 años.",
+    },
 };
-
-
 
 export default function page() {
 
